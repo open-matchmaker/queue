b13d584a3fc9b33902396045da51b1997fa7b581
hans <hans.git@furg.br>
2022-07-23 22:35:27 +0000

    container mongodb

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
-const path = require("path");
-const http = require("http");
-const express = require("express");
-const socketio = require("socket.io");
-const formatMessage = require("./utils/messages");
-const createAdapter = require("@socket.io/redis-adapter").createAdapter;
-const redis = require("redis");
-require("dotenv").config();
-const { createClient } = redis;
-const {
-  userJoin,
-  getCurrentUser,
-  userLeave,
-  getRoomUsers,
-} = require("./utils/users");
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const dotenv = require('dotenv').config({path : '.env'})
 
-const queueLoad = require('./utils/queue')
+const PORT = process.env.PORT || 3000;
+
+const connectDB = require('./scr/database/connection');
+
+const queueLoad = require('./scr/utils/queue')
 
 const app = express();
 const server = http.createServer(app);
@@ -23,14 +17,8 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-const botName = "ChatCord Bot";
-
-/* (async () => {
-  pubClient = createClient({ url: "redis://127.0.0.1:6379" });
-  await pubClient.connect();
-  subClient = pubClient.duplicate();
-  io.adapter(createAdapter(pubClient, subClient));
-})(); */
+//mongodb connection
+connectDB();
 
 // Run when client connects
 io.on("connection", (socket) => {
@@ -48,6 +36,5 @@ io.on("connection", (socket) => {
 
 });
 
-const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
